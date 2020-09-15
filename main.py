09d2f25ab677eb43ef092f3f9fe2b7cfe1faae83
Cavid Aliyev <markmarkul123@gmail.com>
2020-09-15 20:42:29 +0400

    First website

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-# Flaski qowuruq, 2ci import bizim html kodlarimizi bura qowur
 from flask import Flask, render_template, url_for, request, redirect
-# SQL lite i import edirik
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-# burada sag terefde hansi sql ile ve table in adini yaziriq
+# SQLite'ı qoşuruq
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///study.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Study(db.Model):
-    id = db.Column(db.Integer, primary_key=True)  # id reqem ile yazilir ve primary key i var
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     intro = db.Column(db.String(300), nullable=False)  # nullable odur ki intro yeri mecbur dolmalidir
-    text = db.Column(db.Text, nullable=False)  # text geniw cumleler ucun istifade olunur
+    text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)  # datetime library ile default olaraq indiki zamani gosterecek
 
-    # Bu yazi ile biz onu edirikki, biz obyekti cixiwa verende onun id si ile cixiwa vereceyik
+
     def __repr__(self):
         return '<Study %r>' % self.id
 
@@ -37,9 +35,8 @@ def about():
 
 @app.route('/posts')
 def posts():
-    # bu yazi bizim db mizdeki 1ci meqaleni 'meqaleye' elave edir - all yazsaq hamsi elave olunacaq
     meqaleler = Study.query.order_by(Study.date.desc()).all() #desc metodu postlari siraliyir
-    return render_template("posts.html", meqaleler=meqaleler) #biz burda meqalemizi meqaleye menimsetdikki htmlde meqale adi ile muraciet edek
+    return render_template("posts.html", meqaleler=meqaleler) #meqaleler=meqaleler yazısını ona görə yazdımki, template'də db-ə məqaleler adınnan müraciət edim
 
 
 #Meqaleleri id ile siraliyiriq ki her meqalenin oz id si olsun
@@ -51,47 +48,47 @@ def post_detail(id):
 
 @app.route('/posts/<int:id>/delete')
 def post_delete(id):
-    meqale = Study.query.get_or_404(id) #bu ona gore lazimdiki dbde meqale tapilmasa 404 vursun
+    meqale = Study.query.get_or_404(id)
 
     try:
         db.session.delete(meqale)
         db.session.commit()
         return redirect('/posts')
     except:
-        return "Meqaleni silerken xeta bash verdi"
+        return "Qeydi silərkən xəta baş verdi"
 
 
-@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])  # alinan melumatlari get yadaki post yolu ile qowdugumuzu yoxlayiriq
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
 def post_update(id):
-    #dbden melumatlari gotururk cunki yeni bir meqale yazmaga ehtiyac yoxdu olani deyiwmeliyik
+    #dbden melumatlari götürürük çünki yeni bir məqalə yazmaga ehtiyac yoxdur, olanı dəyişməliyik
         meqale = Study.query.get(id)
         if request.method == 'POST':
-            meqale.title = request.form['title']  # create-articledaki titleni bura qowuruq
+            meqale.title = request.form['title']
             meqale.intro = request.form['intro']
             meqale.text = request.form['text']
 
-            # db nin qowulub qowulmadigini yoxlayiriq
+            # db-nin qoşulub qoşulmadığını yoxlayırıq
             try:
 
                 db.session.commit()  # melumatlari yadda saxlayiriq
                 return redirect('/posts')
             except:
-                return "Meqale deyiwikliyi edende sehvlik yarandi"
+                return "Qeyddə dəyişiklik edərkən xəta baş verdi"
 
         else:
             return render_template("post_update.html", meqale=meqale)
 
 
-@app.route('/create-article', methods=['POST', 'GET'])  # alinan melumatlari get yadaki post yolu ile qowdugumuzu yoxlayiriq
+@app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == 'POST':
-        title = request.form['title']  # create-artocledaki titleni bura qowuruq
+        title = request.form['title']
         intro = request.form['intro']
         text = request.form['text']
 
         study = Study(title=title, intro=intro, text=text)
 
-        # db nin qowulub qowulmadigini yoxlayiriq
+        # db nin qoşulub qoşulmadığını yoxlayırıq
         try:
             db.session.add(study)  # add xususi metoddur ve bizim melumatlari db e elave edir
             db.session.commit()  # melumatlari yadda saxlayiriq
@@ -103,6 +100,6 @@ def create_article():
         return render_template("create-article.html")
 
 
-# bu scriptnen gosteririkki esas yer buradi
+
 if __name__ == '__main__':
     app.run(debug=True)
